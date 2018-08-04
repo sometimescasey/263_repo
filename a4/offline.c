c74ff6a9e14bfc3fe2e840c3dfd495607cddd2ce
Casey Li <sometimescasey@gmail.com>
2018-08-04 19:01:23 -0400

    working refactor

--- a4/offline.c
+++ a4/offline.c
@@ -60,15 +60,8 @@ void OffLineMinimum(Item **items, int *k_rep) {
 	printExtracted(extracted);
 }
 
+void parseString(char str[], Item **items, int *k_rep) {
 
-
-int main() {
-	
-
-	Item **items;
-	items = malloc(N_INSERTS * sizeof *items);
-	
-	char str[] = "4, 8, E, 3, E, 9, 2, 6, E, E, E, 1, 7, E, 5";
 	int j_index = 1;
 
 	char *list;
@@ -78,9 +71,6 @@ int main() {
 
 	Item *current;
 	Item *prev = dummyItem(); // avoid segfault
-
-	int *k_rep;
-	k_rep = malloc(8 * sizeof *k_rep);
 
 	while (list != NULL) {
 		printf("%s\n", list);
@@ -123,6 +113,75 @@ int main() {
 		list = strtok(NULL, ", ");
 
 	}
+}
+
+
+int main() {
+
+	int *k_rep;
+	k_rep = malloc(8 * sizeof *k_rep);
+
+	char str[] = "4, 8, E, 3, E, 9, 2, 6, E, E, E, 1, 7, E, 5";
+
+	Item **items;
+	items = malloc(N_INSERTS * sizeof *items);
+	
+	// char str[] = "4, 8, E, 3, E, 9, 2, 6, E, E, E, 1, 7, E, 5";
+	// int j_index = 1;
+
+	// char *list;
+	// list = strtok(str, ", ");
+	// char *endptr;
+	// int digit;
+
+	// Item *current;
+	// Item *prev = dummyItem(); // avoid segfault
+
+
+
+	// while (list != NULL) {
+	// 	printf("%s\n", list);
+	// 	if (strcmp(list, "E") == 0) { // replace with more robust function
+	// 		j_index += 1;
+	// 	} else {
+
+	// 		digit = strtol(list, &endptr, 10);
+	// 		if (endptr == list) {
+	// 			fprintf(stderr, "Parsing problem: strtol encountered a non-digit\n");
+	// 			exit(1);
+	// 		}
+
+	// 		current = makeSet(digit, j_index);
+
+	// 		items[digit-1] = current; // keep pointer for use later
+	// 		printf("stored item with value %d at items[] = %d\n", current->v, digit-1);
+	// 		// subsequent calls must pass null to get the next item
+	// 		printf("fine\n");
+
+	// 			printf("prev->j: %d\n", prev->j);
+	// 			if ((prev->j) == j_index) {
+
+	// 				prev = union_set(current, prev);
+	// 				printf("newset | j: %d --------- \n", prev->j);
+	// 				print(prev);
+	// 				printf("newset end --------- \n");
+	// 				printf("item %d is now in set with rep %d\n", current->v, findSet(current)->v);
+	// 			} else {
+	// 				printf("j changed, stored current->v %d into k_rep[%d]\n", current->v, j_index);
+	// 				prev = current;
+	// 			}
+			
+			
+			
+	// 		printf("j_index: %d\n", j_index);
+	// 		k_rep[j_index] = prev->v;
+	// 	}
+		
+	// 	list = strtok(NULL, ", ");
+
+	// }
+
+	parseString(str, items, k_rep);
 
 	OffLineMinimum(items, k_rep);
 
